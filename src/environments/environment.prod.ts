bb61fe47c838fbfda17714827d6a9bc19b342943
Anshad Vattapoyil <anshadv@somnoware.com>
2017-10-10 18:07:27 +0530

    Fix active content mismatch issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+  baseUrl: 'https://api.openweathermap.org/data/2.5/forecast',
   appId: '78c6b64f54db08d8254473fd536c3bf6',
   cityId: '1277333'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+  baseUrl: 'https://api.openweathermap.org/data/2.5/forecast',
   appId: '78c6b64f54db08d8254473fd536c3bf6',
   cityId: '1277333'
 };
