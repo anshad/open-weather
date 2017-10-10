7e060d224ff661e2e8fb444e4c19451ab971e59b
Anshad Vattapoyil <anshadv@somnoware.com>
2017-10-10 16:39:23 +0530

    App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { WeatherService } from './weather/weather.service';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [WeatherService]
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  weatherForecastData: any[];
+  errorMessage: string;
+
+  constructor(private _weatherService_: WeatherService) { }
+
+  ngOnInit() {
+    this._weatherService_.getWeatherForecast()
+      .subscribe(
+      data => {
+        this.weatherForecastData = data;
+      },
+      err => {
+        this.errorMessage = <any>err;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { WeatherListComponent } from './weather/weather-list/weather-list.component';
+import { MyDatePipe } from './my-date.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    WeatherListComponent,
+    MyDatePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
