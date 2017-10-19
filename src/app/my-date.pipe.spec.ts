b4daba1136f70f354920d158fd10d5cb392547a0
Anshad Vattapoyil <anshad.musafir@gmail.com>
2017-10-19 17:41:35 +0530

    Unit test pipe

--- src/app/my-date.pipe.spec.ts
+++ src/app/my-date.pipe.spec.ts
@@ -1,8 +1,19 @@
 import { MyDatePipe } from './my-date.pipe';
 
 describe('MyDatePipe', () => {
+  let pipe: MyDatePipe;
+
+  beforeEach(() => {
+    pipe = new MyDatePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new MyDatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('format api datetime to correct date', () => {
+    expect(pipe.transform('1508349600').toISOString()).toBe(
+      '2017-10-18T18:00:00.000Z'
+    );
   });
 });
